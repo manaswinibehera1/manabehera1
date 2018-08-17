6e86fafae5f04d4e20200e5a5c9dfb034b6d0c1c
manaswinibehera1 <42205762+manaswinibehera1@users.noreply.github.com>
2018-08-17 09:19:33 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,18 @@
+//first include the library
+#include<iostream>
+using namespace std;
+//writing the function
+int main ()
+{
+//declaring the variables
+int a;
+char b;
+float c;
+bool d;
+//print the result
+cout << "size of int is" << sizeof(a) << endl;
+cout << "size of char is" << sizeof(b) <<endl;
+cout << "size of float is" << sizeof(c) <<endl;
+cout << "size of bool is" << sizeof(d) <<endl;
+return 0;
+}
