a9153c8606afcc902a2569b6453764e2d38d1c07
manaswinibehera1 <42205762+manaswinibehera1@users.noreply.github.com>
2018-08-17 10:37:05 +0530

    Add files via upload

--- lab3_q1.cpp
+++ lab3_q1.cpp
@@ -9,10 +9,12 @@ int a;
 char b;
 float c;
 bool d;
+double e;
 //print the result
-cout << "size of int is" << sizeof(a) << endl;
-cout << "size of char is" << sizeof(b) <<endl;
-cout << "size of float is" << sizeof(c) <<endl;
-cout << "size of bool is" << sizeof(d) <<endl;
+cout << "size of int is " << sizeof(a) << endl;
+cout << "size of char is " << sizeof(b) <<endl;
+cout << "size of float is " << sizeof(c) <<endl;
+cout << "size of bool is " << sizeof(d) <<endl;
+cout << "size of double is " << sizeof(e) <<endl;
 return 0;
 }
